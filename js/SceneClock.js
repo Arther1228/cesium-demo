1d7b8ccb7392f41fd2d440a7ebba5844fb906304
arther606 <396986654@qq.com>
2024-04-20 14:53:03 +0800

    解决冲突

--- js/SceneClock.js
+++ js/SceneClock.js
@@ -18,11 +18,11 @@ class SceneClock {
             this.map.controls.timeline.zoomTo(this.start, this.stop)
         }
 
-        this.map.clock.startTime = this.start.clone()
-        this.map.clock.stopTime = this.stop.clone()
-        this.map.clock.currentTime = this.start.clone()
-        this.map.clock.multiplier = 1 // 当前速度，默认为1
-        this.map.clock.shouldAnimate = false // 是否开启时钟动画，默认true
+        this.map.clock.startTime = this.start.clone();
+        this.map.clock.stopTime = this.stop.clone();
+        this.map.clock.currentTime = this.start.clone();
+        this.map.clock.multiplier = 1; // 当前速度，默认为1
+        this.map.clock.shouldAnimate = false; // 是否开启时钟动画，默认true
         this.map.clock.clockRange = Cesium.ClockRange.UNBOUNDED; // 到达终止时间后循环
 
     }
@@ -38,5 +38,4 @@ class SceneClock {
 
     }
 
-}
 }
