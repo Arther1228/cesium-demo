5ddbda2e46184938c9353158b99a6e7433cc9e78
arther <396986654@qq.com>
2024-04-21 13:50:22 +0800

    电磁干扰写法转换

--- js/InterferenceDrones.js
+++ js/InterferenceDrones.js
@@ -1,55 +1,54 @@
-/**
- * 电磁干扰无人机
- */
-let InterferenceDrones = (function () {
+class InterferenceDrones {
 
-    function InterferenceDrones(options) {
-        this.map = undefined;
-        this.graphicLayer = undefined;
+    /**
+     * 电磁干扰
+     * @param {*} options.map 地图对象 
+     * @param {*} options.graphicLayer 图层
+     * @param {*} options.length 攻击距离 
+     * @param {*} options.vehiclePosition 车辆坐标 
+     */
+    constructor(options) {
 
-        let defaultOptions = {
-            length: 3000, // 攻击距离
-            vehiclePosition: { // 汽车坐标
-                lng: 117.1878,
-                lat: 31.8189,
-                height: 44,
-                heading: 25
+        this.map = options.map;
+        this.graphicLayer = options.graphicLayer;
+        this.length = options.length;
+        this.vehiclePosition = options.vehiclePosition;
+
+        this.directions = [ // 方位
+            {
+                time: '2024-01-22 12:00:15.000',
+                heading: 0,
+                pitch: 30
             },
-            directions: [ // 方位
-                {
-                    time: '2024-01-22 12:00:15.000',
-                    heading: 0,
-                    pitch: 30
-                },
-                {
-                    time: '2024-01-22 12:00:17.000',
-                    heading: 100,
-                    pitch: 40
-                },
-                {
-                    time: '2024-01-22 12:00:20.000',
-                    heading: 150,
-                    pitch: 70
-                }
-            ],
-        }
-
-        options = Object.assign(defaultOptions, options);
-        Object.assign(this, options);
-
-        this.droneTrack = undefined;
-        this.coneTrack = undefined;
-        this.label = undefined;
+            {
+                time: '2024-01-22 12:00:17.000',
+                heading: 100,
+                pitch: 40
+            },
+            {
+                time: '2024-01-22 12:00:20.000',
+                heading: 150,
+                pitch: 70
+            }
+        ];
 
         this.create();
+
         if (!this.positions) { // 如果传递的不是positions，则计算positions
             this.positions = [];
             this.toPositions();
         }
+
         this.move();
+
     }
 
-    InterferenceDrones.prototype.create = function () {
+
+    /**
+     * 创建扫描显示的圆锥追总体
+     */
+    create() {
+
         // 圆锥追踪体
         this.droneTrack = new DroneTrack({
             map: this.map,
@@ -57,6 +56,7 @@ let InterferenceDrones = (function () {
             position: [0, 0, 0],
             targetPosition: [1, 1, 1],
         });
+
         this.coneTrack = this.droneTrack.coneTrack;
 
         // div信息
@@ -71,33 +71,57 @@ let InterferenceDrones = (function () {
         });
     }
 
-    InterferenceDrones.prototype.toPositions = function () {
+
+    /**
+     * 转化为点
+     */
+    toPositions() {
+
         this.directions.map(direction => {
             let position = this.toPosition(direction);
             this.positions.push(position);
         });
+
     }
 
-    InterferenceDrones.prototype.toPosition = function (direction) {
+    /**
+     * 将方位转化为坐标
+     */
+    toPosition(direction) {
+
         let position = {
             time: direction.time
         };
+
         let length = this.length * Math.cos(direction.pitch * Math.PI / 180);
         let pos = getNextPosition(this.vehiclePosition.lng, this.vehiclePosition.lat, direction.heading, length);
         pos = toDegrees(pos);
         position.lng = pos.lng;
         position.lat = pos.lat;
         position.height = this.length * Math.sin(direction.pitch * Math.PI / 180);
+
         return position;
+
     }
 
-    InterferenceDrones.prototype.appendData = function (direction) {
+
+    /**
+     * 动态追加数据
+     */
+    appendData(direction) {
+
         this.directions.push(direction);
         let position = this.toPosition(direction);
         this.positions.push(position);
+
     }
 
-    InterferenceDrones.prototype.move = function () {
+
+    /**
+     * 圆锥追踪体动态效果
+     */
+    move() {
+
         this.coneTrack.position = new Cesium.CallbackProperty((time, result) => {
             let datetime = dayjs(Cesium.JulianDate.toDate(time));
             let startTime = dayjs(string2Date(this.directions[0].time));
@@ -107,12 +131,14 @@ let InterferenceDrones = (function () {
             }
             return Cesium.Cartesian3.fromDegrees(this.vehiclePosition.lng, this.vehiclePosition.lat, this.vehiclePosition.height);
         });
+
         this.coneTrack.targetPosition = new Cesium.CallbackProperty((time, result) => {
             let datetime = dayjs(Cesium.JulianDate.toDate(time));
             let position = findPosition(datetime, this.positions);
             if (!position) return undefined;
             return Cesium.Cartesian3.fromDegrees(position.lng, position.lat, position.height);
         });
+
         this.label.graphic.position = new Cesium.CallbackProperty((time, result) => {
             let datetime = dayjs(Cesium.JulianDate.toDate(time));
             let position = findPosition(datetime, this.positions);
@@ -123,14 +149,18 @@ let InterferenceDrones = (function () {
         });
     }
 
-    InterferenceDrones.prototype.dispose = function () {
+
+    /**
+     * 清理
+     */
+    dispose() {
+
         if (this.droneTrack) {
             this.droneTrack.dispose();
         }
         if (this.label) {
             this.label.dispose();
         }
-    }
 
-    return InterferenceDrones;
-})()
+    }
+}
